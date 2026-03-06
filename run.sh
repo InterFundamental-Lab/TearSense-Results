0c4cd10be70877442a9bb344f987bb34e7a8e1fd
Eric Kang <erickdhdock@gmail.com>
2026-03-06 15:24:05 +1100

    getting rid of .sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ rm -rf outputs/ external_assessor/ __pycache__/
 
 cp -r ../OutputLibraryTearSense/* ./
 
-# source ~/.zshrc
-# conda activate tear_assessor
+source ~/.zshrc
+conda activate tear_assessor
 
-# python run.py
+python run.py
